dcef05cbaf449051675d58754b1a57c947319ead
tyrannojung <tyranno2020@gmail.com>
2023-12-20 14:11:08 +0900

    css update

--- app/(member)/success/page.tsx
+++ app/(member)/success/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
         {showDomain && (
           <div className={styles.center}>
                   <div className={styles.domainSection}>
-                    <p className={styles.domainLabel}>Transaction Link:</p>
+                    <div className={styles.domainLabel}>Transaction Link:</div>
                     <Link href={"https://goerli.lineascan.build/tx/" + session.user.txhash} className={styles.domainLink} target="_blank">
                       {"https://goerli.lineascan.build/tx/" + session.user.txhash}
                     </Link>
